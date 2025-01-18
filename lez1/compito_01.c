367625f394a9e2fdc155b27745cb3c3a186c3162
irene-vallan <vallanirene@gmail.com>
2025-01-18 14:41:54 +0000

    caricamento esercizio

--- lez1/compito_01.c
+++ lez1/compito_01.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{ printf("Enter your name:\t");
+char name[30];
+scanf("%s", name);
+
+printf("\nEnter your surname:\t");
+char surname[30];
+scanf("%s", surname);
+
+printf("\nWelcome, %s %s.\nThank you for using our program.\nWe wish you a merry Christmas!\n\n\n", name, surname);
+
+printf("   *\n  ***\n *****\n*******\n  ***\n\n\n");
+    return 0;
+}
