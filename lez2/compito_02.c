4c50a76914a6910d1b1a43b03d0d945c1eef4ef4
irene-vallan <vallanirene@gmail.com>
2025-01-22 16:53:54 +0000

    modifica commenti

--- lez2/compito_02.c
+++ lez2/compito_02.c
@@ -1,13 +1,16 @@
+/*Stampi un messaggio introduttivo con il proprio nome e cognome
+Chieda all'utente di inserire la categoria e il peso in kg. del dispositivo elettronico da riciclare
+Stampi un messaggio che indichi qual è il peso in kg. che è possibile riciclare e qual è il peso del materiale da buttare*/
+
 // library for the standard input/output
 #include <stdio.h>
-
-//librayr for strings
+//library for strings
 #include <string.h>
 
 //macro that prints name and surname
 #define name printf("Irene Vallan\n\n"); 
 
-//contants for the recycle percentages
+//constants for the recyclable percentages
 #define R1 85*weight/100  
 #define R2 75*weight/100
 #define R3 65*weight/100
@@ -15,13 +18,12 @@
 #define R5 40*weight/100
 
 int main() {
-	//using the macro to print the name
+	//using the macro to print name and surname
 	name
 	
 	//the user inserts the category
-	printf("Inserire la categoria RAEE: R");
-	char category[8];
-	//int category;
+	printf("Inserire la categoria RAEE: ");
+	char category[3];
 	scanf("%s", category);
 
     // the user inserts the weight
@@ -29,40 +31,42 @@ int main() {
 	double weight;
 	scanf("%lf", &weight);
 
-	if (category == 1) {
+   // printing the total weight, the reciclable percentage and the not reciclable percentage based on the category
+	if (strcmp(category, "R1")==0) {
 		printf("\nPeso totale: %.2lf", weight);
 		printf("\nRecuperabile: %.2lf", R1);
 		printf("\nIrrecuperabile: %.2lf", weight - R1);
 	}
 
-	else if (category == 2) {
+	else if (strcmp(category, "R2")==0) {
 		printf("\nPeso totale: %.2lf", weight);
 		printf("\nRecuperabile: %.2lf", R2);
 		printf("\nIrrecuperabile: %.2lf", weight - R2);
 	}
 
-	else if (category == 3) {
+	else if (strcmp(category, "R3")==0) {
 		printf("\nPeso totale: %.2lf", weight);
 		printf("\nRecuperabile: %.2lf", R3);
 		printf("\nIrrecuperabile: %.2lf", weight - R3);
 	}
 
-	else if (category == 4) {
+	else if (strcmp(category, "R4")==0) {
 		printf("\nPeso totale: %.2lf", weight);
 		printf("\nRecuperabile: %.2lf", R4);
 		printf("\nIrrecuperabile: %.2lf", weight - R4);
 	}
 
-	else if (category == 5) {
+	else if (strcmp(category, "R5")==0) {
 		printf("\nPeso totale: %.2lf", weight);
 		printf("\nRecuperabile: %.2lf", R5);
 		printf("\nIrrecuperabile: %.2lf", weight - R5);
 	}
+    //printing "ERROR" if the category inserted by the user is not right
+	else { printf("ERRORE: inserire un'altra categoria"); };
 
-	else { printf("ERRORE"); };
-
-
+    //printing a goodbye message
 	printf("\n\nGrazie per aver usato il nostro programma!\n");
 
+    //indication that the program is terminated successfully
 	return 0;
 }
