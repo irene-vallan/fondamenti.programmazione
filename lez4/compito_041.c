11620fa270a887931a9c57df9e225c9d3e38b127
irene-vallan <vallanirene@gmail.com>
2025-01-22 17:30:53 +0000

    commenti

--- lez4/compito_041.c
+++ lez4/compito_041.c
@@ -1,27 +1,33 @@
+/*Legga da tastiera il giorno della settimana (come stringa) e la spesa effettuata in quel giorno.
+Usi una macro per calcolare l'IVA (22%) sulla spesa.
+Usi una struttura di controllo if-else per distinguere i giorni feriali da quelli festivi.
+Stampi il totale delle spese con e senza IVA, specificando se il giorno è feriale o festivo.*/
+
+//lybrary for the standard input/output
 #include <stdio.h> 
+//library for strings
 #include <string.h> 
 
 // macro that calculate the IVA of the expense
 #define IVA 22*expense/100 
 
 int main(){
-    //insert the day of the week
+    //the user inserts the day of the week
     printf("Inserire il giorno della settimana: "); 
     char day[20];
     scanf("%s", day);
 
-
-    //insert the expense
+    //the user inserts the expense
     printf("\nInserire la spesa effettuata in questo giorno: (euro) "); 
     double expense;
     scanf("%lf", &expense);
 
-    //calculating the total of expense, with and without IVA
+    //printing the total of expense, with and without IVA
     printf("\nSpesa totale senza IVA: %.2lf", expense); 
     printf("\nIVA: %.2lf", IVA);
     printf("\nSpesa totale con IVA: %.2lf", expense+IVA);
 
-
+    //if-else structure to distinguish weekdays from holidays
     if(strcmp(day,"domenica")==0) {
         printf("\n%s è un giorno festivo.\n\n", day);
     }
@@ -29,6 +35,6 @@ int main(){
         printf("\n%s è un giorno feriale.\n\n", day);
     };
 
-
+    //the program is terminated successfully
     return 0;
 }
---
+++ lez4/compito_042.c
@@ -0,0 +1,8 @@
+/*Gestisca le spese mensili per elettricità, acqua, gas e internet.
+Usi una macro per calcolare una penale del 10% per ritardi nei pagamenti.
+Usi direttive come #ifdef e #ifndef per: abilitare o disabilitare il calcolo della penale in base a una configurazione.
+Permettere il debug del programma stampando informazioni aggiuntive.
+Determini se la spesa totale supera una soglia predefinita e stampi un messaggio con if-else.
+Offra suggerimenti di risparmio utilizzando uno switch.
+Stampi un riepilogo finale con tutte le informazioni.*/
+
