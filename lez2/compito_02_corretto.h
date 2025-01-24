1a4ff3b09fa6ae6db8a12c2ac0782db3b3534129
irene-vallan <vallanirene@gmail.com>
2025-01-24 13:48:07 +0000

    costanti

---
+++ lez2/compito_02_corretto.h
@@ -0,0 +1,9 @@
+// printing name and surname
+#define name printf("Irene Vallan\n\n"); 
+
+//constants for the recyclable percentages
+#define R1 85*weight/100  
+#define R2 75*weight/100
+#define R3 65*weight/100
+#define R4 50*weight/100
+#define R5 40*weight/100
