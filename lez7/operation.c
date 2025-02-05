b5654ade70236889f0d15bda7dc54475d9de61b6
irene-vallan <vallanirene@gmail.com>
2025-02-05 17:24:51 +0000

    ok

---
+++ lez7/operation.c
@@ -0,0 +1,14 @@
+int addi(int op1, int op2) {
+    return op1+op2;
+}
+
+int subi(int op1, int op2) {
+    return op1-op2;
+}
+int mul(int op1, int op2) {
+    return op1*op2;
+}
+
+float divi(int op1, int op2) {
+    return op1/op2;
+}
---
+++ lez7/operation.h
@@ -0,0 +1,4 @@
+int addi(int, int);
+int subi(int, int);
+int mul(int, int) ;
+float divi(int, int);
