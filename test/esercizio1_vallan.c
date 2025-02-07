042f468e1de2b472f7f192c557d43483352d403f
irene-vallan <vallanirene@gmail.com>
2025-02-07 15:34:39 +0000

    finito

---
+++ test/esercizio1_vallan.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <math.h>
+#include "macro.h"
+
+int main() {
+    int x=0, y=0;
+    int size=4;
+
+    //richiesta di inserimento di due numeri da tastiera
+    printf("Inserire il primo numero: ");
+     scanf("%d", &x);
+    printf("Inserire il secondo numero: ");
+    scanf("%d", &y);
+
+    //scelta delle operazioni da tastiera
+    printf("1. Addizione (+)\n2. Sottrazione (-)\n3. Moltiplicazione (*)\n4. Divisione (/)\n5. Modulo (%%)\n6. Potenza (^)\n7. Test di tutte le funzioni\n8. Esci\nScegli un'operazione (1-8):");
+    int choice=0;
+    do {
+        scanf("%d", &choice);
+        switch (choice) {
+            //addizione
+            case 1: 
+            printf("%d + %d = %d\n", x, y, add(x, y));
+            break;
+
+            //sottrazione
+            case 2:
+            printf("%d - %d = %d\n", x, y, sub(x, y));
+            break;
+
+            //moltiplicazione
+            case 3: 
+            printf("%d * %d = %d\n", x, y, mul(x, y));
+            break;
+
+            //divisione con messaggio di errore per denominatore = 0
+            case 4:
+            if(y!=0) {
+                printf("%d / %d = %.3f\n", x, y, divi(x, y));
+            }
+            else { 
+                printf("Errore. Denominatore non valido");
+            }
+            break;
+            
+
+            //modulo con messaggio di errore per denominatore=0
+            case 5:
+            if(y!=0) {
+                printf("%d %% %d = %d", x, y, mod(x, y));
+            }
+            else {
+                printf("Errore. Denominatore non valido");
+            }
+            break;
+
+            //potenza
+            case 6:
+            printf("%d ^ %d = %d\n", x, y, power(x, y));
+            break;
+
+            //risultati di tutte le operazioni tra i numeri del vettore
+            case 7:
+            printf("Test delle operazioni sui numeri {2, 4, 6, 8, 10}\n");
+            test_all_operations(numbers, size);
+            break;
+
+            //uscita dal programma
+            case 8:
+            break;
+
+            //messaggio di errore se viene selezionato un caso non valido
+            default:
+            printf("Scelta non valida!\n");
+        }
+    } while (choice!=8);
+
+    return 0;
+}
---
+++ test/funzioni.h
@@ -0,0 +1,50 @@
+//definizioni delle funzioni per le operazioni
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x+y;
+}
+
+int sub(int x, int y) {
+    return x-y;
+}
+
+int mul(int x, int y) {
+    return x*y;
+}
+
+float divi(int x, int y) {
+        float x1=x, y1=y;
+        return x1/y1;
+}
+
+int mod(int x, int y) {
+        return x%y;
+}
+
+//definizione della potenza utilizzando la moltiplicazione
+int p=1;
+int i=0;
+int power(int x, int y) {
+    while(i<y) {
+        p=p*x;
+        i++;
+    }
+    return p;
+}
+
+//definizione della funzione per testare tutte le operazioni
+int numbers[] = {2, 4, 6, 8, 10};
+void test_all_operations(int numbers[], int size) {
+    for (int i=0; i<size-1; i++ ) {
+        int a = numbers[i];
+        int b= numbers[i+1];
+        
+        printf("%d + %d = %d\n", a, b, add(a, b));
+        printf("%d - %d = %d\n", a, b, sub(a, b));
+        printf("%d * %d = %d\n", a, b, mul(a, b));
+        printf("%d / %d = %.3f\n", a, b, divi(a, b));
+        printf("%d %% %d = %d\n", a, b, mod(a, b));
+        printf("%d ^ %d = %d\n", a, b, power(a, b));
+    }
+};
